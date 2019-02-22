41116fdbd9c1c9cf02e0d2c4b80c66ec8f5a2754
Renan Moura <rvmoura.96@gmail.com>
2019-02-22 15:51:42 -0300

    recursive fib .py without tests.

---
+++ exercicios_python_pro/fib_recursive/fib_recursive.py
@@ -0,0 +1,8 @@
+def fib(self, number: int) -> int:
+    if number == 0 or number == 1:
+        return number
+
+    if number > 1:
+        number = self.fib(number - 1) + self.fib(number - 2)
+
+    return number
